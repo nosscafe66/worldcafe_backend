a0ebf53f8f772ab9af8b6ab78ce5d587397bd3ff
nosscafe66 <nosscafe66@gmail.com>
2023-12-26 14:53:37 +0900

    データベース接続先変更

--- linebot/api/server.js
+++ linebot/api/server.js
@@ -1,82 +1,3 @@
-'use strict';
-
-require('dotenv').config();
-
-const express = require('express');
-const line = require('@line/bot-sdk');
-const { Pool } = require('pg');
-const PORT = process.env.PORT || 3000;
-
-const config = {
-    channelSecret: process.env.LINE_CHANNEL_SECRET,
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESSTOKEN
-};
-
-const app = express();
-
-// PostgreSQLデータベースへの接続設定
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-async function insertDataToDatabase(event, text, category) {
-  try {
-    const query = `
-      INSERT INTO linebot_messages (
-        message_id, text, user_id, group_id, timestamp, reply_token, webhook_event_id, mode, is_redelivery, category
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`;
-
-    const values = [
-      event.message.id,
-      text,
-      event.source.userId,
-      event.source.groupId,
-      event.timestamp,
-      event.replyToken,
-      event.webhookEventId,
-      event.mode,
-      event.deliveryContext.isRedelivery,
-      category
-    ];
-
-    await pool.query(query, values);
-  } catch (err) {
-    console.error('Error inserting data to database:', err);
-  }
-}
-
-async function fetchAllData() {
-  try {
-    const { rows } = await pool.query('SELECT * FROM linebot_messages');
-    return rows;
-  } catch (err) {
-    console.error('Error fetching all data from database:', err);
-    throw err;
-  }
-}
-
-app.get('/', async (req, res) => {
-  try {
-    const data = await fetchAllData();
-    res.json("このサーバーはLINE Botのためのものです。");
-  } catch (err) {
-    res.status(500).send('Server error');
-  }
-});
-
-app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
-        .catch((err) => {
-            console.error(err);
-            res.status(500).end();
-        });
-});
-
 const client = new line.Client(config);
 
 async function handleEvent(event) {
@@ -90,21 +11,46 @@ async function handleEvent(event) {
   let textWithoutCategory = event.message.text;
 
   if (matches) {
-    // カテゴリのテキストを取得
     categoryText = matches[1];
-
-    // メッセージからカテゴリのテキストを取り除く
     textWithoutCategory = event.message.text.replace(categoryRegex, '').trim();
   }
 
-  // カテゴリを含むイベントデータをデータベースに保存（カテゴリ除外済みのテキストを使用）
-  await insertDataToDatabase(event, textWithoutCategory, categoryText);
+  let userName = '';
+  if (event.source.type === 'user') {
+    try {
+      const profile = await client.getProfile(event.source.userId);
+      userName = profile.displayName;
+    } catch (error) {
+      console.error('Error getting user profile:', error);
+    }
+  }
+
+  await insertDataToDatabase(event, textWithoutCategory, categoryText, userName);
 }
 
-if (process.env.NOW_REGION) {
-    module.exports = app;
-} else {
-    app.listen(PORT, () => {
-        console.log(`Server running at ${PORT}`);
-    });
+async function insertDataToDatabase(event, text, category, userName) {
+  try {
+    const query = `
+      INSERT INTO linebot_messages (
+        message_id, text, user_id, user_name, group_id, timestamp, reply_token, webhook_event_id, mode, is_redelivery, category
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`;
+
+    const values = [
+      event.message.id,
+      text,
+      event.source.userId,
+      userName,
+      event.source.groupId,
+      event.timestamp,
+      event.replyToken,
+      event.webhookEventId,
+      event.mode,
+      event.deliveryContext.isRedelivery,
+      category
+    ];
+
+    await pool.query(query, values);
+  } catch (err) {
+    console.error('Error inserting data to database:', err);
+  }
 }
